191de913f8608a2ce160680d91f04576ff9dd8a5
KNME <g.giorgobiani@gtu.ge>
2017-09-02 00:31:32 +0400

    Add files via upload

---
+++ modules/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+cout << "\033[0d\033[2J";
+cout << "Hello World!\n";
+
+return 0;
+
+}
---
+++ modules/world.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+
+{
+
+cout << "using c++\n" << endl;
+cout << "Line-1\n" << endl;
+
+printf("using c function\t");
+printf("Line-2\n");
+
+return 0;
+
+}
